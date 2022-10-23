695b52be8502090fa8d0fd06cc203323154a8ab6
iotHong <86907571+iotHong@users.noreply.github.com>
2022-10-23 10:51:49 +0800

    Update Q2_sql.sql

--- Q2_sql.sql
+++ Q2_sql.sql
@@ -1,8 +1,8 @@
 SELECT Id, ShipCountry,case
-									WHEN ShipCountry IN( 'USA','Mexico','Canada') 
-									THEN 'NorthAmerica'
-									ELSE 'OtherPlace' 
-									END
+						WHEN ShipCountry IN( 'USA','Mexico','Canada') 
+						THEN 'NorthAmerica'
+						ELSE 'OtherPlace' 
+						END AS Area
 FROM "Order"
 WHERE Id >= 15445
 ORDER BY  Id
