23a106f1b7c595f774829933343d90dd8b1b994c
iotHong <86907571+iotHong@users.noreply.github.com>
2022-10-23 14:53:13 +0800

    Update Q5_DISCONTINUED.sql

--- Q5_DISCONTINUED.sql
+++ Q5_DISCONTINUED.sql
@@ -1,10 +1,10 @@
 SELECT ProductName, CompanyName, ContactName
 FROM (SELECT CustomerId, ProductId, ProductName, MIN(OrderDate)
-		  FROM OrderDetail ordet
+	FROM OrderDetail ordet
 	        JOIN Product pd ON pd.Id = ordet.ProductId
 	        JOIN "Order" od ON od.Id = ordet.OrderId
-	    WHERE pd.Discontinued = 1
-	    GROUP BY ProductId ) A
+	WHERE pd.Discontinued = 1
+	GROUP BY ProductId ) A
 
 JOIN Customer c ON A.CustomerId = c.Id 
 
