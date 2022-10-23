f49bbc0c4fdc37e9e29f9eb1c623f135e09135c5
iotHong <86907571+iotHong@users.noreply.github.com>
2022-10-23 16:22:12 +0800

    Update Q9_CHRISTMAS.sql

--- Q9_CHRISTMAS.sql
+++ Q9_CHRISTMAS.sql
@@ -1,7 +1,8 @@
 WITH ProductName AS (SELECT DISTINCT ProductName
-										 FROM 'Order'
+		     FROM 'Order'
+		     
                        JOIN Product ON Product.Id=OrderDetail.ProductId
-										   JOIN OrderDetail ON OrderDetail.OrderId='Order'.Id
+		       JOIN OrderDetail ON OrderDetail.OrderId='Order'.Id
                        JOIN Customer ON Customer.Id='Order'.CustomerId
 										 
                      WHERE CompanyName='Queen Cozinha' AND OrderDate LIKE '2014-12-25%'
