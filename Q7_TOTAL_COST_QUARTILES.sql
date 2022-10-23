8375699715efd1cab40ee3a5fb15dcc692733542
iotHong <86907571+iotHong@users.noreply.github.com>
2022-10-23 15:44:07 +0800

    Update Q7_TOTAL_COST_QUARTILES.sql

--- Q7_TOTAL_COST_QUARTILES.sql
+++ Q7_TOTAL_COST_QUARTILES.sql
@@ -3,7 +3,7 @@
 SELECT CompanyName,CustomerId,TotalExpenditures
 FROM (SELECT*, ntile(4) OVER(ORDER BY TotalExpenditures) AS result
 			FROM (SELECT ifnull(CompanyName, 'MISSING_NAME') AS CompanyName, 
-																		CustomerId, round(sum(UnitPrice*Quantity),2) AS TotalExpenditures
+						CustomerId, round(sum(UnitPrice*Quantity),2) AS TotalExpenditures
             FROM 'Order' join OrderDetail on OrderDetail.OrderId='Order'.Id
                        left join Customer on 'Order'.CustomerId=Customer.Id
             GROUP BY CustomerId))
