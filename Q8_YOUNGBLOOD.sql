919a172a4f7d9348aa3b71fda0ab69a58d7bda95
iotHong <86907571+iotHong@users.noreply.github.com>
2022-10-23 15:59:58 +0800

    Update Q8_YOUNGBLOOD.sql

--- Q8_YOUNGBLOOD.sql
+++ Q8_YOUNGBLOOD.sql
@@ -1,6 +1,6 @@
 SELECT RegionDescription AS Region, FirstName, LastName, max(BirthDate) AS BirthDate
 FROM Employee JOIN EmployeeTerritory ON Employee.Id=EmployeeTerritory.EmployeeId
               JOIN Territory ON EmployeeTerritory.TerritoryId=Territory.Id
-							JOIN Region ON Region.Id=Territory.RegionId
+	      JOIN Region ON Region.Id=Territory.RegionId
 GROUP BY Region.Id
 ORDER BY Region.Id;
