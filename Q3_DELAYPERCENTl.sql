14c71e84dedc7c1eb518c274cd4d3d33eeebddae
iotHong <86907571+iotHong@users.noreply.github.com>
2022-10-23 16:26:10 +0800

    Update Q3_DELAYPERCENTl.sql

--- Q3_DELAYPERCENTl.sql
+++ Q3_DELAYPERCENTl.sql
@@ -1,5 +1,4 @@
-SELECT companyname, 
-				ROUND(COUNT(CASE WHEN ShippedDate > RequiredDate THEN true ELSE NULL END) * 100.00/ count(*), 2) AS Percent
+SELECT companyname, ROUND(COUNT(CASE WHEN ShippedDate > RequiredDate THEN true ELSE NULL END) * 100.00/ count(*), 2) AS Percent
 FROM 'Order', 'Shipper'
 WHERE 'Order'.ShipVia = Shipper.id
 GROUP BY Shipper.id
