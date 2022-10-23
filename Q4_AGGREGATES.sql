0829311a2e9e685c2e8d29c48da982667af5829e
iotHong <86907571+iotHong@users.noreply.github.com>
2022-10-23 12:15:57 +0800

    Create Q4_AGGREGATES.sql

---
+++ Q4_AGGREGATES.sql
@@ -0,0 +1,11 @@
+SELECT categoryname, COUNT(*) as  Number, 
+										round(avg(product.unitprice), 2) AS Avg,
+										min(product.unitprice) AS Min, 
+										max(product.unitprice) AS Max,
+										sum(product.unitsonorder) AS TotalUnitsOnOrder
+										
+FROM product, category
+WHERE category.id = product.categoryid 
+GROUP BY categoryname
+HAVING count(category.id) > 10
+ORDER BY category.id;
